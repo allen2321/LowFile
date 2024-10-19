c3a0e42ec1d67f9e50b427ad1c78f63da6a120a4
DHZHZ <Allenzual@gmail.com>
2024-10-19 02:50:11 -0600

    Problemas con el wsam

--- Hackathon/Low-File/low_file_contract/app/src/lib.rs
+++ Hackathon/Low-File/low_file_contract/app/src/lib.rs
@@ -1,39 +1,47 @@
 #![no_std]
 
-// necesary crates
+// Importa las dependencias necesarias
 use sails_rs::prelude::*;
 
-// import our modules 
+// Importa los módulos 
 pub mod states;
 pub mod services;
 
-// Import service to be used for the program
-use services::traffic_light_service::LowFileService;
+// Importa el servicio que se va a utilizar en el programa
+use services::low_file_service::LowFileService;  // Cambiado a LowFileService
 
-// Traffic light program struct to build the program (there can only be one per contract)
-pub struct TrafficLightProgram;
 
-// Traffic light program, it host one or more services and it expose them to the 
-// externar consumer.
-// Only one program is allowed per application
+// Programa principal del contrato
+pub struct LowFileProgram;
+
+// Programa LowFile, puede alojar uno o más servicios y los expone al consumidor externo.
+// Solo se permite un programa por aplicación
 #[program]
-impl TrafficLightProgram {
-    // Application constructor (it is an associated function)
-    // It can be called once per application lifetime.
+impl LowFileProgram {
+    // Constructor de la aplicación (función asociada)
+    // Se puede llamar una vez por ciclo de vida de la aplicación
     pub fn new() -> Self {
-        // Init the state
-        //LowFileService::seed();
-
+        // Inicializa el estado;  // Inicializa el servicio LowFile
         Self
     }
 
-    // Method working with "&self", having no other parameters are treated as exposed
-    // service constructors, and are called each time when an incoming request message 
-    // needs be dispatched to a selected service
-    // It has "message routing", This will change the way a service will be called 
-    // (if omitted, the method name will be used, in this case TrafficLightSvc).
-    #[route("TrafficLight")]
-    pub fn traffic_light_svc(&self) -> LowFileService {
-        LowFileService::new("".to_string(), "".to_string(), 0, "".to_string(), "".to_string(), "".to_string(), Vec::new(),"".to_string(), Vec::new(),"".to_string(),"".to_string())
+    // Método que trabaja con "&self", y sin otros parámetros, se trata como un 
+    // constructor de servicio expuesto, llamado cada vez que un mensaje de solicitud 
+    // necesita ser despachado a un servicio seleccionado
+    #[route("LowFileService")]  // Ajustado a LowFileService
+    pub fn low_file_svc(&self) -> LowFileService {
+        LowFileService::new(
+            "".to_string(), 
+            "".to_string(), 
+            0, 
+            "".to_string(), 
+            "".to_string(), 
+            "".to_string(), 
+            Vec::new(),
+            "".to_string(), 
+            "".to_string().into(),
+            "".to_string(),
+            "".to_string(),
+        )
     }
 }
--- Hackathon/Low-File/low_file_contract/wasm/build.rs
+++ Hackathon/Low-File/low_file_contract/wasm/build.rs
@@ -1,36 +1,32 @@
 use sails_client_gen::ClientGenerator;
 use std::{env, fs, path::PathBuf};
-use app::TrafficLightProgram;
+use app::LowFileProgram;  // Aseguramos que el programa utilizado es LowFileProgram
 
 fn main() {
-    // Build contract to get .opt.wasm
+    // Compilar el contrato para obtener el archivo .opt.wasm
     sails_rs::build_wasm();
 
-    // Path where the file "Cargo.toml" is located (points to the root of the project)
-    // 'CARGO_MANIFEST_DIR' specifies this directory in en::var
+    // Ruta donde se encuentra el archivo "Cargo.toml"
     let cargo_toml_path = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap());
 
-    // Path where the client will be generated 
-    // 'OUT_DIR' points to a temporary directory used by the compiler 
-    // to store files generated at compile time. 
+    // Ruta donde se generará el cliente (en OUT_DIR)
     let outdir_path = PathBuf::from(env::var("OUT_DIR").unwrap());
 
-    // Path where the file "app.idl" will be created
+    // Ruta donde se creará el archivo "app.idl"
     let idl_path = cargo_toml_path.clone().join("app.idl");
     let client_path = outdir_path.clone().join("app_client.rs");
 
-    // This generate the contract IDL
-    sails_idl_gen::generate_idl_to_file::<TrafficLightProgram>(idl_path.clone())
-        .unwrap();
+    // Generar la IDL del contrato
+    sails_idl_gen::generate_idl_to_file::<LowFileProgram>(idl_path.clone())  // Cambiado a LowFileProgram
+        .expect("Fallo al generar la IDL");
 
-    // Generator of the clients of the contract
-    /*ClientGenerator::from_idl_path(&idl_path)
+    // Si deseas generar el cliente del contrato, descomenta esta sección
+    /*
+    ClientGenerator::from_idl_path(&idl_path)
         .generate_to(client_path.clone())
-        .unwrap();*/
+        .expect("Fallo al generar el cliente");
+    */
 
-    // Then, copies the client that is in the OUT_DIR path in the current directory (wasm), where the 
-    // "Cargo.toml" file is located 
-    //fs::copy(client_path, cargo_toml_path.join("app_client.rs"))
-    //    .unwrap();
+    // Copiar el cliente al directorio actual donde se encuentra "Cargo.toml"
+   
 }
-*
